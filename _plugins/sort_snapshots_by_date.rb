1b033682f0e6064d1d07109250378c90cb917fd4
Luke Chadwick <luke@vertis.io>
2024-03-08 14:25:15 +0000

    feat: grouped and styled

--- _plugins/sort_snapshots_by_date.rb
+++ _plugins/sort_snapshots_by_date.rb
@@ -1,37 +1,32 @@
 require 'nokogiri'
 require 'yaml'
 Jekyll::Hooks.register :site, :after_init do |_site, _payload|
-  puts "Sorting snapshots by date..."
+  puts "Sorting snapshots by date and grouping..."
   # Define the paths
-  html_folder = "snapshots"  # Replace with your actual static files folder if different
-  yaml_file = "_data/sorted_snapshot_dates.yml"  # This is where the YAML file will be saved
+  html_folder = "snapshots"
+  yaml_file = "_data/sorted_snapshot_dates.yml"
 
-  # Initialize an empty array to store filenames with dates
-  snapshots_with_dates = []
+  # Initialize a hash to store filenames grouped by dates
+  snapshots_grouped_by_dates = Hash.new { |hash, key| hash[key] = [] }
 
   # Loop through each HTML file in the directory
   Dir["#{html_folder}/*.html"].each do |filepath|
     filename = File.basename(filepath)
 
     # Extract date from filename
-    date_match = filename.match(/-(\d{4}-\d{2}-\d{2}T\d{2}_\d{2}_\d{2}.\d{3}Z)/)
-    date = date_match ? date_match[1] : nil
+    date_match = filename.match(/[\-_](\d{4}-\d{2}-\d{2})T(\d{2})_(\d{2})_(\d{2}\.\d{3})Z/)
+    date = date_match ? date_match[1] : "0000-00-00"
 
-    # Convert the date string to an actual DateTime object for sorting, if date is not nil
-    if date
-      date = DateTime.parse(date.gsub('_', ':'))
-    else
-      # Assign a very old date to ensure files without a date are sorted to the start of the list
-      date = DateTime.new(0)
-    end
+    # Convert the date string to an actual Date object for sorting, if date is not nil
+    date = Date.parse(date)
 
-    # Add to array
-    snapshots_with_dates << { filename: filename, date: date }
+    # Group by date
+    snapshots_grouped_by_dates[date] << filename
   end
 
-  # Sort the array by date, ensuring nil dates are handled
-  sorted_snapshots = snapshots_with_dates.sort_by { |snapshot| snapshot[:date] }.map { |snapshot| snapshot[:filename] }
+  # Sort the hash by date in reverse order and transform it into a hash
+  sorted_and_grouped_snapshots = snapshots_grouped_by_dates.sort_by { |date, _filenames| date }.reverse.to_h
 
-  # Write sorted array to a YAML file
-  File.open(yaml_file, 'w') { |file| file.write(sorted_snapshots.to_yaml) }
+  # Write sorted and grouped hash to a YAML file
+  File.open(yaml_file, 'w') { |file| file.write(sorted_and_grouped_snapshots.to_yaml) }
 end
